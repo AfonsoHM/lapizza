de2bae52cca699a0195b18e272c4061e7adc6a6c
Afonso Monteiro <affonsoh.monteiro@gmail.com>
2025-06-29 21:23:04 -0300

    criando componente pizza e formatando preço

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
+import { Prisma } from "@/generated/prisma"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function decimalToMoney(price: string | number | Prisma.Decimal) {
+  return parseFloat(price.toString()).toLocaleString('pt-br', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  } )
 }
